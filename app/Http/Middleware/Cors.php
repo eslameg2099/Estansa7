a3f1a3cd6b750f0a7287851cda6b47e5a7e45ac8
eslameg <eslameg2099@gmail.com>
2023-01-01 00:58:20 +0200

    add

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Origin', 'https://estansa7.com')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers: *');
+
     }
 }
