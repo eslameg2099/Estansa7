8816379874a0b3936963b1761363c13a76b6b020
eslameg <eslameg2099@gmail.com>
2023-01-04 20:29:14 +0200

    add

--- config/mail.php
+++ config/mail.php
@@ -70,7 +70,8 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+
 
     /*
     |--------------------------------------------------------------------------
