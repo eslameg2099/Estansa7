aa01c8f834b5d694e6a5b57325fc63412bc525f0
eslameg <eslameg2099@gmail.com>
2023-01-12 19:11:23 +0200

    add

--- resources/views/dashboard/coupons/partials/actions/show.blade.php
+++ resources/views/dashboard/coupons/partials/actions/show.blade.php
@@ -1,13 +1,9 @@
 @if(method_exists($coupon, 'trashed') && $coupon->trashed())
-    @can('view', $coupon)
         <a href="{{ route('dashboard.coupons.trashed.show', $coupon) }}" class="btn btn-outline-dark btn-sm">
             <i class="fas fa fa-fw fa-eye"></i>
         </a>
-    @endcan
 @else
-    @can('view', $coupon)
         <a href="{{ route('dashboard.coupons.show', $coupon) }}" class="btn btn-outline-dark btn-sm">
             <i class="fas fa fa-fw fa-eye"></i>
         </a>
-    @endcan
 @endif
