0fce3bc1d475b4e4af9a639459d02a682ea5a6ca
eslameg <eslameg2099@gmail.com>
2023-01-25 11:49:41 +0200

    add

--- app/Http/Filters/ProviderFilter.php
+++ app/Http/Filters/ProviderFilter.php
@@ -71,8 +71,8 @@ class ProviderFilter extends BaseFilters
     {
         if ($value) {
             if (Str::contains($value, ',')) {
-                $from = explode(',', $value)[0];
-                $to = explode(',', $value)[1];
+                $from = explode(',', $value)[1];
+                $to = explode(',', $value)[2];
                 $this->builder->whereBetween('unit_price', [$from, $to]);
             } else {
                 $this->builder->where('unit_price', $value);
