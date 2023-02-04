57dbbefde161c694b3ed378d728d2abae50756a2
eslameg <eslameg2099@gmail.com>
2023-02-04 16:12:23 +0200

    add

--- resources/views/dashboard/accounts/providers/partials/flags/phone.blade.php
+++ resources/views/dashboard/accounts/providers/partials/flags/phone.blade.php
@@ -1,6 +1,6 @@
 <div class="d-inline-flex align-items-center">
     <span class="mx-2">{{ $provider->phone }}</span>
-    @if($provider->phone_verified_at)
+    @if($provider->phone_verified_at != null && $provider->provider_verified_at != null)
         <span class="badge badge-success">@lang('users.verified')</span>
     @else
         <span class="badge badge-warning">@lang('users.unverified')</span>
