4ffaf106955b2e66aebf427bacdd905074561564
eslameg <eslameg2099@gmail.com>
2023-02-06 20:04:23 +0200

    add

--- resources/views/dashboard/reservations/show.blade.php
+++ resources/views/dashboard/reservations/show.blade.php
@@ -91,13 +91,13 @@
                     </tr>
                     @if($reservation->excuse != null)
                     <tr>
-                        <th width="200">@lang('reservations.attributes.created_at'):</th>
+                        <th width="200">@lang('reservations.excuse.reason'):</th>
                         <td>{{ $reservation->excuse->reason}}</td>
                         
                     </tr>
 
                     <tr>
-                        <th width="200">@lang('reservations.attributes.created_at'):</th>
+                        <th width="200">@lang('reservations.excuse.comment'):</th>
                         <td>{{ $reservation->excuse->comment}}</td>
                         
                     </tr>
