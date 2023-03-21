2d1099c0628d306289003834b418de5473705814
eslameg2099 <eslameg2099@yahoo.com>
2023-03-21 20:15:01 +0200

    add

--- resources/views/emails/mymail.blade.php
+++ resources/views/emails/mymail.blade.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 
-  <h3>{{ $details['title'] }}</h3>
+  <h3>{{ $details['user'] }}</h3>
   <img src="https://backend.estansa7.com/storage/8/logoee193b16.png"  width="250" height="250">
 
    
   
 
-    <a href="{{url('https://est.ragabkalbida.com/api/verification/verify/'.$details['body'])}}">للتفعيل اضغط هنا</a>
+    <a href="{{url('https://est.ragabkalbida.com/api/verification/verify/'.$details['user'])}}">للتفعيل اضغط هنا</a>
     
     <p>Thank you {{ $details['user'] }} for use estansa7.com  </p>
 </body>
