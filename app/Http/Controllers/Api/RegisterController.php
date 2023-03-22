400605c8bf6b40180527e9e859d4e31110837d06
eslameg2099 <eslameg2099@yahoo.com>
2023-03-22 12:52:09 +0200

    add

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -117,7 +117,13 @@ class RegisterController extends Controller
         ], [
             'code' => rand(111111, 999999),
         ]);
-        $response = Http::post('https://ulfa.d.deli.work/api/sendmail', $data = [
+
+        $details =[
+            'user'=> $user->name,
+        ];
+
+        \Mail::to($user->email)->send(new \App\Mail\email($details));
+   /*     $response = Http::post('https://ulfa.d.deli.work/api/sendmail', $data = [
             'user' => $verification->code,
             'code'=> $verification->code,
             'name'=>$user->name,
@@ -125,7 +131,7 @@ class RegisterController extends Controller
             'type'=>'active',
 
 
-        ]);
+        ]); */
 
 
       
