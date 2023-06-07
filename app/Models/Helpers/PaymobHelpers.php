bf7fd9a3135d3d750c7533a60859784d25a69453
unknown <eslameg2099@yahoo.com>
2023-06-07 21:32:15 +0300

    add_check

--- app/Models/Helpers/PaymobHelpers.php
+++ app/Models/Helpers/PaymobHelpers.php
@@ -54,7 +54,6 @@ class PaymobHelpers
     ]);
 
    $response_final_final_json = $response_final_final->json();
-   return $response_final_final_json;
    $res = "https://accept.paymobsolutions.com/api/acceptance/iframes/" . $iframe . "?payment_token=" . $response_final_final_json['token'];
    return $res;
 
