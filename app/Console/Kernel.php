eaca9e10ef4288ff70fe7fc56169e7e4b8140706
unknown <eslameg2099@yahoo.com>
2023-06-17 20:51:31 +0300

    ad

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('Daily:Reservation')->daily();
+
     }
 
     /**
@@ -36,7 +38,6 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-        $schedule->command('Daily:Reservation')->daily();
         
 
         require base_path('routes/console.php');
