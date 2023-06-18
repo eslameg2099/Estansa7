d26cde682bf9641a563f7f6fc8b4290e51d4f680
unknown <eslameg2099@yahoo.com>
2023-06-18 19:58:11 +0300

    ad

--- app/Http/Resources/ReservationResource.php
+++ app/Http/Resources/ReservationResource.php
@@ -45,6 +45,8 @@ class ReservationResource extends JsonResource
             'discount'=> new price($this->discount),
             'created_at' => new Date( $this->created_at),
             'free'=>true,
+            'wait_time' => Carbon::parse($reservation->from)->format('h:i') - Carbon::parse(today())->format('h:i'),
+
 
         ];
     }
