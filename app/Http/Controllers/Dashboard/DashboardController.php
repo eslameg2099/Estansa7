f0ef626585c1985edbbbff1bed26475109da44ec
unknown <eslameg2099@yahoo.com>
2023-06-19 11:59:57 +0300

    Ad

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -17,11 +17,10 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return Carbon::now()->toTimeString();
         $reservations = Reservation::with('customer','category','provider')
         ->whereDate('day_at', today())
         ->where('stauts','2')
-        ->where('from',Carbon::now()->toDateTimeString())
+        ->where('from',Carbon::now()->toTimeString())
         ->get();
         return $reservations;
         $Reservations = Reservation::with('customer','category','provider')
