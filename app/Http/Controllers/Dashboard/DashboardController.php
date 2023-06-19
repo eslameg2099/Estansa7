dcf9ab6bd71267b1ae957c3658c614f85bccc863
unknown <eslameg2099@yahoo.com>
2023-06-19 12:49:03 +0300

    Ad

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends Controller
         ->where('stauts','2')
         ->WhereTime('from',Carbon::now()->format('h:i:s'))
         ->get();
-        return $reservations;
+        return now();
         $Reservations = Reservation::with('customer','category','provider')
         ->whereDate('day_at', today())
         ->where('stauts','2')
