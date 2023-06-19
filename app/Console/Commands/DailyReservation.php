e2209a2d3b429f2185b8fa505891dbf8ae4ab3dd
unknown <eslameg2099@yahoo.com>
2023-06-19 14:23:44 +0300

    Ad

--- app/Console/Commands/DailyReservation.php
+++ app/Console/Commands/DailyReservation.php
@@ -55,7 +55,7 @@ class DailyReservation extends Command
                 'title'=>'نذكرك بموعد جلسة اليوم',
                 'date'=> Carbon::parse($reservation->day_at)->toDateString(),
                 'from'=>$reservation->from,
-                'nm'=>'2',
+                'nm'=>'1',
 
                ]); 
 
@@ -67,7 +67,7 @@ class DailyReservation extends Command
                 'title'=>'نذكرك بموعد جلسة اليوم',
                 'date'=> Carbon::parse($reservation->day_at)->toDateString(),
                 'from'=> $reservation->from,
-                'nm'=>'2',
+                'nm'=>'1',
 
 
                ]); 
