0dd8f6688bc8700923560c4f36723434f3101325
unknown <eslameg2099@yahoo.com>
2023-07-04 11:49:47 +0300

    ad

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {  
 
-      return  $request->ip;
+      return  $request->ip();
         $CategoryProviders = CategoryProvider::active()->inRandomOrder()->limit(6)->get();
 
         $Providers_computerscience = Provider::active()->whereHas('categories', function ($builder) {
