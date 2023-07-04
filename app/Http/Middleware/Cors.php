5bdb0aa81302711472fba375d455f939062f7c3b
unknown <eslameg2099@yahoo.com>
2023-07-04 13:53:35 +0300

    ad

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,15 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-       
+        $domains = ['http://localhost:8080'];
 
+        if(isset($request->server()['HTTP_ORIGIN'])){
+            $origin = $request->server()['HTTP_ORIGIN'];
+            if(in_array($origin, $domains)){
+                header('Access-Control-Allow-Origin: '. $origin);
+                header('Access-Control-Allow-Headers: Origin, Content-type, Authorization');
+            }
+        }
         return $next($request);
 
     }
