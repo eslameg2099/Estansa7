aca64e1ee122ae87b8d4bd6107266716ce9896d9
unknown <eslameg2099@yahoo.com>
2023-07-04 13:55:22 +0300

    ad

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -27,6 +27,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {  
+        return $request->server()['HTTP_ORIGIN'];
         $CategoryProviders = CategoryProvider::active()->inRandomOrder()->limit(6)->get();
 
         $Providers_computerscience = Provider::active()->whereHas('categories', function ($builder) {
