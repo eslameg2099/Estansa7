12bf9cc922333e33fbd7a4b27c9921aa673d09a4
unknown <eslameg2099@yahoo.com>
2023-07-04 14:54:04 +0300

    ad

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['https://estansa7.com'],
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://estansa7.com'],
+    'allowed_origins' => ['*.https://estansa7.com'],
 
     'allowed_origins_patterns' => [],
 
