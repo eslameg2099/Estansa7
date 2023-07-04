e62979a2d03ffd2be62ff4a6e44d2d719d82cf0a
unknown <eslameg2099@yahoo.com>
2023-07-04 12:06:01 +0300

    ad

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,23 +16,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $allowedHosts = explode(',', env('ALLOWED_DOMAINS'));
-
-        $requestHost = parse_url($request->headers->get('origin'),  PHP_URL_HOST);
-
-        if(!app()->runningUnitTests()) {
-            if(!\in_array($requestHost, $allowedHosts, false)) {
-                $requestInfo = [
-                    'host' => $requestHost,
-                    'ip' => $request->getClientIp(),
-                    'url' => $request->getRequestUri(),
-                    'agent' => $request->header('User-Agent'),
-                ];
-                event(new UnauthorizedAccess($requestInfo));
-
-                throw new SuspiciousOperationException('This host is not allowed');
-            }
-        }
+      
 
         return $next($request);
     }
