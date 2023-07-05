c8cde9e8d3fa831a9be34919e66a7cbd6be7c380
unknown <eslameg2099@yahoo.com>
2023-07-05 14:04:14 +0300

    fg

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,21 +16,6 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        
-        $allowedOrigins = ['estansa7.com', 'example1.com', 'example2.com'];
-        $origin = $request->server('HTTP_ORIGIN');
-    
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-                ->header('Access-Control-Allow-Headers', 'Content-Type');
-        }
-        else
-        {
-            return 123;
-        }
-    
         return $next($request);
     }
 }
