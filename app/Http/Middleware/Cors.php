ce0099d84086cf97b1bd9b9f808685ce9b3fac15
unknown <eslameg2099@yahoo.com>
2023-07-05 13:46:11 +0300

    fg

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,13 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-      
+        if(request()->headers->get('referer') !=  'estansa7.com' )
+        {
+            return response()->json([
+                'message' => "sorry cant access !",
+            ],404);
+        } 
+        
         return $next($request); 
     }
 }
