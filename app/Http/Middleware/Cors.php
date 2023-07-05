d34b969c7df1e074a09a5ab514417d8a6e8b11a0
unknown <eslameg2099@yahoo.com>
2023-07-05 13:57:10 +0300

    fg

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -26,6 +26,10 @@ class Cors
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
                 ->header('Access-Control-Allow-Headers', 'Content-Type');
         }
+        else
+        {
+            return 123;
+        }
     
         return $next($request);
     }
