4116ed8326d1a9c47b646232bfa665c56671d6a3
unknown <eslameg2099@yahoo.com>
2023-07-05 13:55:09 +0300

    fg

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -27,7 +27,6 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {         
-      return  request()->headers->get('referer') ;
         $CategoryProviders = CategoryProvider::active()->inRandomOrder()->limit(6)->get();
 
         $Providers_computerscience = Provider::active()->whereHas('categories', function ($builder) {
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,6 +17,16 @@ class Cors
     public function handle($request, Closure $next)
     {
         
-        return $next($request); 
+        $allowedOrigins = ['estansa7.com', 'example1.com', 'example2.com'];
+        $origin = $_SERVER['HTTP_ORIGIN'];
+    
+        if (in_array($origin, $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', 'Content-Type');
+        }
+    
+        return $next($request);
     }
 }
